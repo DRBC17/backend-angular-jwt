465b99dd690f7d83ac01d8aa35eaaf02f7923ceb
Diego Buezo <diegobuezo2016@gmail.com>
2020-10-12 01:50:17 -0600

    Se agrego morgan y cors
    Signed-off-by: Diego Buezo <diegobuezo2016@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
 const usersRouter = require("./routes/user.router");
 const app = express();
 
 require("./config/database");
-
+// Middlewares
+const corsOptions = {
+  // origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
